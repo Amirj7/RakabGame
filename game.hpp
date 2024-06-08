ec592b294ba135d9e6135e5e1a51d7627744ed17
unknown <amirrst82>
2024-06-08 03:08:52 -0700

    game class changed

--- game.hpp
+++ game.hpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include "player.hpp"
 
-using namespace std;
-
-class Game 
+class Game : public Player
 {
-    public :
+    public:
+        void run(){
+            std::cout << "Welcome to the game!" << std::endl;
+            std::cout << "Please enter the number of players : ";
 
+            std::cin >> playerNum;
+            if(check_Player_Num(playerNum) == false){
+                std::cout << "Invalid number of players. Please enter a number between 3 and 6 : ";
+                std::cin >> playerNum;
+            }
+
+            Player players[playerNum];
+
+            while(playerNum){
+                for(int i{} ; i < playerNum ; i++){
+                    std::cout << "please enter player" << i << "' name : ";
+                    
+                }
+            }
+        }
     private:
-    
+        int playerNum;
 };
