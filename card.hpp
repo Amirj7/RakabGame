5500ae43cf966b185233104d08c807f6e6454588
unknown <amirrst82>
2024-06-08 04:41:34 -0700

    card

---
+++ card.hpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include "yellow1.hpp"
+#include "yellow2.hpp"
+#include "yellow3.hpp"
+#include "yellow4.hpp"
+#include "yellow5.hpp"
+#include "yellow6.hpp"
+#include "yellow10.hpp"
+
+
+using namespace std;
+
+class card
+{
+public:
+  
+
+private:
+   
+};
