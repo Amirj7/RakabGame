3ee1bb1b05443d734caa806f9df73b88db0e1e94
Amir.j <154706087+Amirj7@users.noreply.github.com>
2024-06-09 14:23:31 +0330

    card and deck changed

--- card.hpp
+++ card.hpp
@@ -1,91 +1,11 @@
 #include <iostream>
-#include <vector>
-#include "yellow1.hpp"
-#include "yellow2.hpp"
-#include "yellow3.hpp"
-#include "yellow4.hpp"
-#include "yellow5.hpp"
-#include "yellow6.hpp"
-#include "yellow10.hpp"
-#include "matarsag.hpp"
-#include "parchamdar.hpp"
-#include "shirdokht.hpp"
-#include "shirzan.hpp"
-#include "spring.hpp"
-#include "winter.hpp"
+#include "player.hpp"
+#include "deck.hpp"
 
-class card : public Yellow1 , public Yellow2 , public Yellow3 , public Yellow4 , public Yellow5 , public Yellow6 ,public Yellow10 ,public Matarsag , public parcahmdar , public shirdokht , public shirzan , public spring , public winter
+class Deck : public Player , protected card
 {
     public:
-        void all_Cards()
-        {
-            Yellow1 y1;
-            for(int i{} ; i < y1.get_num_Of_Card() ; i++){
-                cards.push_back("yellow1");
-            }
 
-            Yellow2 y2;
-            for(int i{} ; i < y2.get_num_Of_Card() ; i++){
-                cards.push_back("yellow2");
-            }
+    private:
 
-            Yellow3 y3;
-            for(int i{} ; i < y3.get_num_Of_Card() ; i++){
-                cards.push_back("yellow3");
-            }
-
-            Yellow4 y4;
-            for(int i{} ; i < y4.get_num_Of_Card() ; i++){
-                cards.push_back("yellow4");
-            }
-
-            Yellow5 y5;
-            for(int i{} ; i < y5.get_num_Of_Card() ; i++){
-                cards.push_back("yellow5");
-            }
-
-            Yellow6 y6;
-            for(int i{} ; i < y6.get_num_Of_Card() ; i++){
-                cards.push_back("yellow6");
-            }
-
-            Yellow10 y10;
-            for(int i{} ; i < y10.get_num_Of_Card() ; i++){
-                cards.push_back("yellow10");
-            }
-
-            Matarsag M1;
-            for(int i{} ; i < M1.get_num_Of_Card() ; i++){
-                cards.push_back("Matarsag");
-            }
-
-            parcahmdar P1;
-            for(int i{} ; i < P1.get_num_Of_Card() ; i++){
-                cards.push_back("parchamdar");
-            }
-
-            shirdokht Sd1;
-            for(int i{} ; i < Sd1.get_num_Of_Card() ; i++){
-                cards.push_back("shirdokht");
-            }
-
-            shirzan Sz1;
-            for(int i{} ; i < Sz1.get_num_Of_Card() ; i++){
-                cards.push_back("shirzan");
-            }
-
-            spring S1;
-            for(int i{} ; i < S1.get_num_Of_Card() ; i++){
-                cards.push_back("spring");
-            }
-
-            winter W1;
-            for(int i{} ; i < W1.get_num_Of_Card() ; i++){
-                cards.push_back("winter");
-            }
-
-        }
-
-    protected:
-        std::vector <std::string> cards;
 };
