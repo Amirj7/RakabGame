3d66fe00e212a98abefa1f4b32c31ef2f8c5243b
Amirhossein Rostami <amir.rst134999@gmail.com>
2024-08-01 07:18:33 -0700

    ENNA and LIA changed their places

--- map.hpp
+++ map.hpp
@@ -11,18 +11,18 @@ public:
     CityMap()
     {
         nearbyCities["BELLA"] = {"CALINE", "PLADACI", "BORGE"};
-        nearbyCities["CALINE"] = {"BALLA", "BORGE", "ENNA", "ATELA", "PLADACI"};
-        nearbyCities["ENNA"] = {"CALINE", "ATELA", "BORGE", "DAMASE"};
-        nearbyCities["ATELA"] = {"CALINE", "ENNA", "DIMASE", "ATHENS"};
+        nearbyCities["CALINE"] = {"BALLA", "BORGE", "LIA", "ATELA", "PLADACI"};
+        nearbyCities["LIA"] = {"CALINE", "ATELA", "BORGE", "DAMASE"};
+        nearbyCities["ATELA"] = {"CALINE", "LIA", "DIMASE", "ATHENS"};
         nearbyCities["PLADACI"] = {"BALLA", "CALINE", "BORGE", "MORINA", "ROLLO"};
-        nearbyCities["BORGE"] = {"BALLA", "CALINE", "ENNA", "DIMASE", "PLADACI", "MORINA", "OLIVADI"};
-        nearbyCities["DIMASE"] = {"ATELA", "ENNA", "BORGE", "OLIVADI", "ATHENS"};
+        nearbyCities["BORGE"] = {"BALLA", "CALINE", "LIA", "DIMASE", "PLADACI", "MORINA", "OLIVADI"};
+        nearbyCities["DIMASE"] = {"ATELA", "LIA", "BORGE", "OLIVADI", "ATHENS"};
         nearbyCities["MORINA"] = {"PLADACI", "BORGE", "OLIVADI", "ARMENTO", "TALMONE", "ROLLO"};
-        nearbyCities["OLIVADI"] = {"DIMASE", "BORGE", "MORINA", "ARMENTO", "LIA"};
+        nearbyCities["OLIVADI"] = {"DIMASE", "BORGE", "MORINA", "ARMENTO", "ENNA"};
         nearbyCities["ROLLO"] = {"PLADACI", "MORINA", "TALMONE", "ELINIA"};
         nearbyCities["TALMONE"] = {"MORINA", "ARMENTO", "ROLLO", "ELINIA"};
-        nearbyCities["ARMENTO"] = {"MORINA", "OLIVADI", "LIA", "TALMONE"};
-        nearbyCities["LIA"] = {"OLIVADI", "ARMENTO"};
+        nearbyCities["ARMENTO"] = {"MORINA", "OLIVADI", "ENNA", "TALMONE"};
+        nearbyCities["ENNA"] = {"OLIVADI", "ARMENTO"};
         nearbyCities["ELINIA"] = {"ROLLO", "TALMONE"};
         nearbyCities["ATHENS"] = {"ATELA", "DIMASE"};
     }
